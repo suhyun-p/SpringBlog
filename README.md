f35ca038ea4075a931aa28ed54d6bf481f31529b
Park Suhyun <suhpark@ebay.com>
2018-07-18 13:27:00 +0900

    스프링 부트(Spring Boot)의 세가지 뷰 (JSP, Velocity, Thymeleaf)

--- README.md
+++ README.md
@@ -12,3 +12,7 @@ Spring Boot 웹 프로젝트 시작하기
 ### Blog_Develop_02 
 http://millky.com/@origoni/post/1101  
 Spring Boot 와 Spring Data (JPA) 를 이용한 간단한 DB 입출력
+
+### Blog_Develop_04
+http://millky.com/@origoni/post/1144  
+스프링 부트(Spring Boot)의 세가지 뷰 (JSP, Velocity, Thymeleaf)
---
+++ src/main/java/com/spring/springblog/presentation/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.spring.springblog.presentation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")
+    public String index(Model model){
+        model.addAttribute("name", "SpringBlog From Suhpark");
+        return "hello";
+    }
+}
